606f08667c2f584747c57f85f0187b016c5926d7
Samuel Beland-Leblanc <samuel.beland.leblanc@gmail.com>
2020-02-06 16:49:17 -0500

    Adjusted for multi GPU

--- stylecorrection/models/transformer.py
+++ stylecorrection/models/transformer.py
@@ -9,13 +9,13 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
-        self.pe = torch.zeros(max_len, d_model)
+        pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        self.pe[:, 0::2] = torch.sin(position * div_term)
-        self.pe[:, 1::2] = torch.cos(position * div_term)
-        self.pe = self.pe.unsqueeze(0)
-        self.register_buffer('pc_pe', self.pe)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
 
     def forward(self, x, offsets=None):
         if offsets is not None:
