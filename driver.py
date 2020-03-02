eefa7cd2924b230719cc544e74493dc1f375e5a8
Samuel Beland-Leblanc <samuel.beland.leblanc@gmail>
2020-03-02 12:17:00 -0500

    Forgot to comment test stuff

--- driver.py
+++ driver.py
@@ -19,48 +19,48 @@ params = parser.parse_args()
 with open(params.config, 'r') as in_file:
     config = yaml.load(in_file, Loader=yaml.FullLoader)
 
-with h5py.File('temp/models/bcu_enwiki_50k_mf2_s0_vocab.h5') as h5_file:
-    vocab = h5_file['vocab'][:]
-
-cl = H5CorpusLoader.load_and_split(
-    'temp/datasets/simple_wiki.h5',
-    use_split_id=0,
-    forced_vocab=vocab
-)
-
-model = TransformerS2S(
-    len(vocab),
-    768,
-    8,
-    4096,
-    6,
-    6
-)
-
-with open('temp/models/test.pkl', 'rb') as in_file:
-    loaded_data = torch.load(in_file, map_location=device)
-    model.load_state_dict(loaded_data['model_state_dict'])
-
-model.eval()
-
-for ds, cs in zip(config['sample_corrections']['dirty'] + ["it is the first time for me to come here ."], config['sample_corrections']['clean']+ ["i hope to hear from you soon ."]):
-    test_sentence = cl.encode_sentence(ds)
-
-    with torch.no_grad():
-        res = model.beam_decode(
-            test_sentence,
-            torch.tensor([cl.bos_idx], dtype=torch.long),
-            beam_width=5,
-            max_len=len(test_sentence)+5,
-            end_token=cl.eos_idx
-        )
-
-    decoded = cl.decode_tensor(torch.tensor(res, dtype=torch.long))
-    # scores = compute_scores([decoded[0].split(' ')[1:-1]], [cs.split(' ')])
-    print('[{}] -> [{}] ({})'.format(ds, decoded, cs))
-    # print(scores)
-
-exit()
+# with h5py.File('temp/models/bcu_enwiki_50k_mf2_s0_vocab.h5') as h5_file:
+#     vocab = h5_file['vocab'][:]
+#
+# cl = H5CorpusLoader.load_and_split(
+#     'temp/datasets/simple_wiki.h5',
+#     use_split_id=0,
+#     forced_vocab=vocab
+# )
+#
+# model = TransformerS2S(
+#     len(vocab),
+#     768,
+#     8,
+#     4096,
+#     6,
+#     6
+# )
+#
+# with open('temp/models/test.pkl', 'rb') as in_file:
+#     loaded_data = torch.load(in_file, map_location=device)
+#     model.load_state_dict(loaded_data['model_state_dict'])
+#
+# model.eval()
+#
+# for ds, cs in zip(config['sample_corrections']['dirty'] + ["it is the first time for me to come here ."], config['sample_corrections']['clean']+ ["i hope to hear from you soon ."]):
+#     test_sentence = cl.encode_sentence(ds)
+#
+#     with torch.no_grad():
+#         res = model.beam_decode(
+#             test_sentence,
+#             torch.tensor([cl.bos_idx], dtype=torch.long),
+#             beam_width=5,
+#             max_len=len(test_sentence)+5,
+#             end_token=cl.eos_idx
+#         )
+#
+#     decoded = cl.decode_tensor(torch.tensor(res, dtype=torch.long))
+#     # scores = compute_scores([decoded[0].split(' ')[1:-1]], [cs.split(' ')])
+#     print('[{}] -> [{}] ({})'.format(ds, decoded, cs))
+#     # print(scores)
+#
+# exit()
 
 if config['mode'] == 'hd5_gen':
     print('Creating hd5 dataset...')
