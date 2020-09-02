06e04ef3c9876446ab215b800b0b3df7b981def5
Samuel Beland-Leblanc <samuel.beland.leblanc@gmail>
2020-09-02 13:34:45 -0400

    Bug fix

--- stylecorrection/operation/eval.py
+++ stylecorrection/operation/eval.py
@@ -33,7 +33,7 @@ class ManualEvaluationOperation(Operation):
             use_split_id=man_eval_conf.hd5_dataset.valid_split_id,
             forced_vocab=(vocab, vocab_special_chars)
         )[0]
-        criterion_placeholder = nn.CrossEntropyLoss(ignore_index=self.cl_train.pad_idx)
+        criterion_placeholder = nn.CrossEntropyLoss(ignore_index=self.cl.pad_idx)
         pretrained_mdl_path = os.path.expandvars(man_eval_conf.pretrained_model)
         self.load_model(len(vocab), criterion_placeholder, pretrained_fn=pretrained_mdl_path)
 
